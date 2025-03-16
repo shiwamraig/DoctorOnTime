db8e33682edfea0fb0a61be4fa8f0644203af078
Pranjalary <pranjalkumar2738@gmail.com>
2025-03-17 04:49:25 +0530

    Mobile view

--- frontend/src/components/SpecialityMenu.jsx
+++ frontend/src/components/SpecialityMenu.jsx
@@ -9,8 +9,8 @@ const SpecialityMenu = () => {
         <p className='sm:w-1/3 text-center text-sm'>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
         <div className='flex sm:justify-center text-center gap-5 pt-5 w-full overflow-scroll'>
           {specialityData.map((items,index)=>(
-            <Link onClick={()=>scrollTo(0,0)} className='felx flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' key={index} to={`/doctors/${items.speciality}`}>
-            <img className='w-16 sm:w-24 mb-2' src={items.image} alt="" />
+            <Link onClick={()=>scrollTo(0,0)} className='flex flex-col items-center text-xs cursor-pointer flex-shrink-0 hover:translate-y-[-10px] transition-all duration-500' key={index} to={`/doctors/${items.speciality}`}>
+            <img className='w-16 sm:w-20 mb-2' src={items.image} alt="" />
             <p>{items.speciality}</p>
             </Link>
           ))}
