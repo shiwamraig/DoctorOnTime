8f261a21928f4bb900a5f557b15cbd461e6d4b32
Pranjalary <pranjalkumar2738@gmail.com>
2025-03-16 15:45:51 +0530

    Appointment page and login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Login = () => {
+  
+  const [state,setState] = useState('Sign Up')
+  
+  const [email,setEmail] = useState('')
+  const [password,setPassword] = useState('')
+  const [name,setName] = useState('')
+
+  const onSubmitHandler = async (event) => {
+          event.preventDefault()
+  }
+
   return (
+
+   <form className='min-h-[80vh] flex items-center'>
+
     <div>
-        
+      <p>{state === 'Sign Up' ? "Create Account" : "Login"}</p>
     </div>
+
+   </form>
   )
 }
 
