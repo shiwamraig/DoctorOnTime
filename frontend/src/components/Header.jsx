e229d449469be8e4e3a660f0a45cb8a35783ab1a
Shiwam Rai <93027661+shiwamraig@users.noreply.github.com>
2025-03-19 07:35:20 +0530

    Refactor Header component for improved formatting and consistency

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -8,15 +8,15 @@ const Header = () => {
 
         <div className='md:w-1/2 flex flex-col items-start justify-center gap-4 py-10 m-auto md:py-[10vw] mb:mb-[-30px]'>
             <p className='text-3xl md:text-4xl lg:text-5xl text-white font-semibold leading-tight md:leading-tight lg:leading-tight '>
-                Book Appointment <br /> With Trusted Doctors
+                 Book Appointment <br /> With Trusted Doctors
                 </p> 
                 <div className='flex flex-col md:flex-row items-center gap-3 text-white text-sm font-light'>
-                    <img className='w-28' src={assets.group_profiles} alt="" />
-                <p>Simply browse through our extensive list of trusted doctors,<br className='hidden sm:block'/>
+                     <img className='w-28' src={assets.group_profiles} alt="" /> 
+                 <p>*Simply browse through our extensive list of trusted doctors,<br className='hidden sm:block'/> 
                 schedule your appointment hassle-free.</p>
                 </div>
-                <a href="#speciality" className='flex items-center gap-2 bg-white px-8 py-3 rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>
-                    Book appointment <img className='w-3' src={assets.arrow_icon} alt="" />
+                 <a href="#speciality" className='flex items-center gap-2 px-8 py-3 bg-white rounded-full text-gray-600 text-sm m-auto md:m-0 hover:scale-105 transition-all duration-300'>
+                 Book appointment <img className='w-3' src={assets.arrow_icon} alt="" /> 
                 </a>
 
             </div>
